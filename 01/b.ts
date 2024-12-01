b9ce9816f35b0d3ed9bf5df12f17a99f20b7c526
Arjan Blok <arjan.blok@mendix.com>
2024-12-01 21:48:58 +0100

    day1

---
+++ 01/b.ts
@@ -0,0 +1,25 @@
+import fs from "node:fs";
+
+const data = fs.readFileSync("01/input.txt", { encoding: "utf8" });
+var leftNrs: number[] = [];
+var rightNrs: number[] = [];
+
+data
+  .split("\n")
+  .map((line) => line.split("   "))
+  .forEach((value) => {
+    leftNrs.push(parseInt(value[0]));
+    rightNrs.push(parseInt(value[1]));
+  });
+
+var total = 0;
+
+leftNrs.forEach((leftNr) =>
+  rightNrs.forEach((rightNr) => {
+    if (rightNr === leftNr) {
+      total = total + leftNr;
+    }
+  })
+);
+
+console.log(total);
